1cc409dc96ef76314980d79b077232def0a772ea
your name here <theo@dali.dartmouth.edu>
2019-02-04 10:55:50 -0800

    add incrementing to state to count up

--- frontend/components/counter.jsx
+++ frontend/components/counter.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 class Counter extends React.Component {
   constructor(props) {
     super(props)
+    this.incrementCounter = this.incrementCounter.bind(this)
     this.state = {
       counter: 0
     }
@@ -12,11 +13,21 @@ class Counter extends React.Component {
     return this.state.counter;
   }
 
+  incrementCounter() {
+    console.log('gets clicked');
+    this.setState({
+      counter: this.state.counter += 1
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>hello wurld</h1>
         <h2>counter: {this.getCount()}</h2>
+        <button onClick={this.incrementCounter}>
+          click me to add!
+        </button>
       </div>
     )
   }
