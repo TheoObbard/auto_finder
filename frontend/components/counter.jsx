7b0c39a0effdd3b34536df20501e4668aae62321
your name here <theo@dali.dartmouth.edu>
2019-02-04 10:48:36 -0800

    add simple logic to display counter

--- frontend/components/counter.jsx
+++ frontend/components/counter.jsx
@@ -3,15 +3,20 @@ import React from 'react';
 class Counter extends React.Component {
   constructor(props) {
     super(props)
-    // this.state = {
-    //   counter: 0
-    // }
+    this.state = {
+      counter: 0
+    }
+  }
+
+  getCount() {
+    return this.state.counter;
   }
 
   render() {
     return (
       <div>
         <h1>hello wurld</h1>
+        <h2>counter: {this.getCount()}</h2>
       </div>
     )
   }
