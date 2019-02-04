324bbdef73d07688bad5048a4ff223a0315a728d
your name here <theo@dali.dartmouth.edu>
2019-02-04 15:06:38 -0800

    try to get pins to drop when clicked

--- frontend/components/map.jsx
+++ frontend/components/map.jsx
@@ -2,11 +2,43 @@ import React from 'react';
 import Pin from './pin';
 
 class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this._onMouseMove = this._onMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+    this.pins = []
+  }
+
+  _onMouseMove(e) {
+    // console.log('mouse position = ' + this.state.x, this.state.y);
+    
+    this.setState({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY });    
+  }
+
+  handleClick(e) {     
+    console.log(this.state.x, this.state.y);
+       
+    this.pins.push(
+      <Pin pos={[this.state.x, this.state.y]}/>
+    )
+  }
+
+  fetchPins() {    
+    this.pins.forEach(pin => {
+      return pin;
+    })    
+  }
 
   render() {
+    const pins = this.pins.forEach(pin => {
+      return pin;
+    })  
+
     return (
-      <div className='map'>
-        <Pin />
+      <div className='map' onClick={() => this.handleClick()} onMouseMove={this._onMouseMove.bind(this)}>
+        <ul>
+          {this.pins}
+        </ul>
         <div className='title'>Welcome to world traveler</div>
         <p className='instructions'>Please mark locations on the map to tell the world where you've been!</p>
       </div>
--- frontend/components/pin.jsx
+++ frontend/components/pin.jsx
@@ -2,19 +2,30 @@ import React from 'react';
 
 class Pin extends React.Component {
   constructor(props) {
-    super(props)
-
+    super(props)    
+    this.state = {
+      // pinX: this.props.pos[0],
+      // pinY: this.props.pos[1] 
+      pinX: 100,
+      pinY: 100 
+    }
   }
 
   render() {
+    console.log(this.props);
+    
     let styles = {
-      top: '400px',
-      left: '200px'
+      top: this.state.pinY + 'px',
+      left: this.state.pinX + 'px'
     };
-    return (
-      <div className='pin' style={styles}>
-      </div>
-    )
+    if (this.props.X && this.props.Y) {
+      return (
+        <div onClick={this.handleClick()} className='pin' style={styles}>
+        </div>
+      )
+    } else {
+      return null;
+    }
   }
 }
 
