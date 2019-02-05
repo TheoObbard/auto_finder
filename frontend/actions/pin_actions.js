dc2a84da26d427bc4c9924c0a091579a4cb6580d
your name here <theo@dali.dartmouth.edu>
2019-02-05 08:44:14 -0800

    fix bug

--- frontend/actions/pin_actions.js
+++ frontend/actions/pin_actions.js
@@ -2,7 +2,7 @@ import * as PinApiUtil from '../util/pin_api_util';
 export const RECEIVE_PIN = 'RECEIVE_PIN';
 
 export const addPin = (x, y) => dispatch => {
-  return PostsApiUtil.addPin(x, y).then(
+  return PinApiUtil.addPin(x, y).then(
     (pin) => dispatch(receivePin(pin))
   )
 };
